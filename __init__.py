0c1245d01592deb61c224109324480ca3a0ea777
such-a-git <adamperrett3142@gmail.com>
2019-02-13 16:58:43 +0000

    removed the bad breakout init stuff in pendulum, bad fix

--- __init__.py
+++ __init__.py
@@ -11,17 +11,3 @@ AbstractSpiNNakerCommon.register_binary_search_path(os.path.dirname(binaries_pat
 # This adds the model binaries path to the paths searched by sPyNNaker
 # ef = executable_finder.ExecutableFinder(os.path.dirname(binaries_path))
 # executable_finder.add_path(os.path.dirname(binaries_path))
-# from spinn_utilities import executable_finder
-# from model_binaries import __file__ as binaries_path
-# from python_models.breakout import Breakout
-# # from visualiser.visualiser import Visualiser
-# # from visualiser.visualiser_subsamp import Visualiser_subsamp
-# import os
-#
-# from spynnaker.pyNN.abstract_spinnaker_common import AbstractSpiNNakerCommon
-#
-# AbstractSpiNNakerCommon.register_binary_search_path(os.path.dirname(binaries_path))
-#
-# # This adds the model binaries path to the paths searched by sPyNNaker
-# # ef = executable_finder.ExecutableFinder(os.path.dirname(binaries_path))
-# # executable_finder.add_path(os.path.dirname(binaries_path))
